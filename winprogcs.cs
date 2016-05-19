35c73fca0b455c665f6d7e988875665ebc502eb4
Chunhui Han <hanhunhui@gmail.com>
2016-05-19 13:09:04 -0700

    new file:   winprogcs.cs

---
+++ winprogcs.cs
@@ -0,0 +1,16 @@
+// C# program
+//
+
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+public class WinHello
+{
+    public static void Main()
+    {
+        MessageBox.Show("Hello","hello",MessageBoxButtons.OKCancel,MessageBoxIcon.Asterisk);
+    }
+}
